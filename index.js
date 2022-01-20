2b054e799567d90660b9e036e5c0953ec9178097
Maxvyr <maxvid30@gmail.com>
2022-01-20 09:35:31 +0100

    :tada: bot for auto send ether or wich crypto on EVM platform

---
+++ index.js
@@ -0,0 +1,34 @@
+import { ethers } from "ethers"; 
+
+const provider = new ethers.providers.JsonRpcProvider("RINKEBY URL"); //infura, alchemy ...
+const receiver = "Address receive Ether";
+
+const privateKeys = ["",""];
+
+//auto send value from this address to good one
+const botSend = async () => {
+    provider.on("block", async () => {
+        console.log("Listen New Block,🙈 waiting 🙈");
+        privateKeys.forEach((privateKey){
+            const _target = new ethers.Wallet(privateKey);
+            const target = _target.connect(provider);
+            const balance = await provider.getBalance(target.address); //balance wallet
+            const tx = ethers.utils.parseEther(".005") //gas for transfer
+            if (balance.sub(tx) > 0){
+                console.log("Ether Find on account 🥳")
+                const amount = balance.sub(tx); //amount to send ether - gas;
+                try {
+                    await target.sendTransaction({
+                        to: receiver,
+                        value: amount,
+                    })
+                    console.log(`✅ Transfer Success ✅ value -> ${ethers.utils.formatEther(balance)}`)
+                } catch (e){
+                    console.error("error", e);
+                }
+            }
+        });
+    });
+}
+
+botSend();
